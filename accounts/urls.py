75fca3ce43b4d8229fbb34a32db45f6bfdb8e4f5
Dery047 <vergarasantiago049@gmail.com>
2025-07-06 19:24:57 -0500

    new route: VerifyEmail in urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import RegisterView, LoginView, LogoutView
+from .views import RegisterView, LoginView, LogoutView, VerifyEmail
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('verify-email/', VerifyEmail.as_view(), name='verify-email'),
 ]
