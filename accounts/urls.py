6383e116bee4265e44c1d95e904f5f546088f4b4
Dery047 <vergarasantiago049@gmail.com>
2025-07-07 22:24:46 -0500

    new url: active user; for front end verification

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import RegisterView, LoginView, LogoutView, VerifyEmail
+from .views import RegisterView, LoginView, LogoutView, activate_user_view
+from .VerifyEmail import VerifyEmailView
+
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('verify-email/', VerifyEmail.as_view(), name='verify-email'),
+    path('verify-email/', VerifyEmailView.as_view(), name='verify-email'),
+    path('activate-user/', activate_user_view, name='activate-user'),
+
 ]
